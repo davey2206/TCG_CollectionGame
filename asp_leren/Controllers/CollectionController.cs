92d4012f9ae7646a6e3ffe13b7cc1f976ff40128
davey van den eijnde <davey2206@LIVE.NL>
2022-03-08 10:48:03 +0100

    -added home page -moved old home page to collection page -added background images

---
+++ asp_leren/Controllers/CollectionController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PokemonTcgSdk;
+
+namespace asp_leren.Controllers
+{
+    public class CollectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["sets"] = getSets();
+            return View();
+        }
+
+        public IActionResult Collection(string sName)
+        {
+            ViewData["sets"] = getSets();
+            ViewData["cards"] = getCards(sName);
+            return View();
+        }
+
+        public List<string> getSets()
+        {
+            var sets = Sets.All();
+            while (sets == null || sets.Count == 0)
+            {
+                sets = Sets.All();
+            }
+            List<string> lSets = new List<string>();
+            foreach (var set in sets)
+            {
+                lSets.Add(set.Name);
+            }
+
+            return lSets;
+        }
+
+        public List<string> getCards(string sName)
+        {
+            var sets = Sets.All();
+            string code = null;
+            foreach (var set in sets)
+            {
+                if (set.Name == sName)
+                {
+                    code = set.Code;
+                }
+            }
+            Dictionary<string, string> query = new Dictionary<string, string>()
+            {
+                { "setCode", code }
+            };
+            var cards = Card.Get(query);
+            List<string> lCards = new List<string>();
+            foreach (var card in cards.Cards)
+            {
+                lCards.Add(card.ImageUrl);
+            }
+
+            return lCards;
+        }
+    }
+}
--- asp_leren/Controllers/HomeController.cs
+++ asp_leren/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using PokemonTcgSdk;
+
 
 namespace asp_leren.Controllers
 {
@@ -14,46 +14,6 @@ namespace asp_leren.Controllers
     {
         public IActionResult Index()
         {
-            var sets = Sets.All();
-            while (sets == null || sets.Count == 0)
-            {
-                sets = Sets.All();
-            }
-            List<string> lSets = new List<string>();
-            foreach (var set in sets)
-            {
-                lSets.Add(set.Name);
-            }
-            ViewData["sets"] = lSets;
-            return View();
-        }
-
-        public IActionResult Collection(string sName)
-        {
-            var sets = Sets.All();
-            while (sets == null || sets.Count == 0)
-            {
-                sets = Sets.All();
-            }
-            string code = null;
-            foreach (var set in sets)
-            {
-                if (set.Name == sName)
-                {
-                    code = set.Code;
-                }
-            }
-            Dictionary<string, string> query = new Dictionary<string, string>()
-            {
-                { "setCode", code }
-            };
-            var cards = Card.Get(query);
-            List<string> lCards = new List<string>();
-            foreach (var card in cards.Cards)
-            {
-                lCards.Add(card.ImageUrl);
-            }
-            ViewData["cards"] = lCards;
             return View();
         }
     }
